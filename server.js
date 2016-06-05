e188c88c336ca8d0770fcd81fe9c172c97b5da70
vinaypuppal <puppalvinay@gmail.com>
2016-06-05 11:09:35 +0530

    basic server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var express = require('express');
+var morgan = require('morgan');
+var router = require('./router');
+var path = require('path');
+var app = express();
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,'/client')));
+router(app);
+
+var server = http.createServer(app);
+
+var port = process.env.PORT || 3000;
+
+server.listen(port,function(error){
+	if(error) throw error;
+	console.log("Server running at port "+port);
+});
